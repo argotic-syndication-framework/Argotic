f747a7c252793c9f9c169f8d653ee080e437d427
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2026-01-26 09:49:54 +0000

    Upgrade to ZF

---
+++ .zf/config.ps1
@@ -0,0 +1,72 @@
+<#
+This example demonstrates a software build process using the 'ZeroFailed.Build.DotNet' extension
+to provide the features needed when building a .NET solutions.
+#>
+
+$zerofailedExtensions = @(
+    @{
+        # References the extension from its GitHub repository. If not already installed, the latest version from 'main' will be downloaded.
+        Name = "ZeroFailed.Build.DotNet"
+        GitRepository = "https://github.com/zerofailed/ZeroFailed.Build.DotNet"
+        GitRef = "main"
+    }
+    @{
+        # References the extension from its GitHub repository. If not already installed, the latest version from 'main' will be downloaded.
+        Name = "ZeroFailed.Build.GitHub"
+        GitRepository = "https://github.com/zerofailed/ZeroFailed.Build.GitHub"
+        GitRef = "main"
+    }
+)
+
+# Load the tasks and process
+. ZeroFailed.tasks -ZfPath $here/.zf
+
+#
+# Build process configuration
+#
+#
+# Build process control options
+#
+$SkipInit = $false
+$SkipVersion = $false
+$SkipBuild = $false
+$CleanBuild = $Clean
+$SkipTest = $false
+$SkipTestReport = $false
+$SkipAnalysis = $false
+$SkipPackage = $false
+$SkipPublish = $false
+
+$SolutionToBuild = (Resolve-Path (Join-Path $here "./Solutions/Argotic.slnx")).Path
+$ProjectsToPublish = ""
+$NugetPublishSource = property ZF_NUGET_PUBLISH_SOURCE "$here/_local-nuget-feed"
+$IncludeAssembliesInCodeCoverage = "Argotic.*"
+$ExcludeAssembliesInCodeCoverage = "Argotic.Extensions.Tests"
+
+$CreateGitHubRelease = $true
+$PublishNuGetPackagesAsGitHubReleaseArtefacts = $true
+
+task . FullBuild
+
+#
+# Build Process Extensibility Points - uncomment and implement as required
+#
+
+# task RunFirst {}
+# task PreInit {}
+# task PostInit {}
+# task PreVersion {}
+# task PostVersion {}
+# task PreBuild {}
+# task PostBuild {}
+# task PreTest {}
+# task PostTest {}
+# task PreTestReport {}
+# task PostTestReport {}
+# task PreAnalysis {}
+# task PostAnalysis {}
+# task PrePackage {}
+# task PostPackage {}
+# task PrePublish {}
+# task PostPublish {}
+# task RunLast {}
